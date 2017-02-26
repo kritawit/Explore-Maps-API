9fb680197dfdfab4335673a897ec889b11996f9c
Header-Develop <kritawitbunket@gmail.com>
2017-02-26 13:52:48 +0700

    Created positioned the map so it has the user's tap in the center. -Created an animation so the map moves gradually.

--- exploreMapAPI/ViewController.swift
+++ exploreMapAPI/ViewController.swift
@@ -12,11 +12,39 @@ import MapKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myMapOUTLET: MKMapView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    @IBAction func tapOnMap(_ sender: UITapGestureRecognizer) {
+
+        if sender.state == .ended {
+
+            let touchLocationAsPoint: CGPoint = sender.location(in: view)
+            let touchLocationAsCoodinate: CLLocationCoordinate2D = myMapOUTLET.convert(touchLocationAsPoint, toCoordinateFrom: view)
+
+            UIView.animate(withDuration: 1.5, animations: {
+
+                self.myMapOUTLET.centerCoordinate = touchLocationAsCoodinate
+
+            })
+
+        }
+
     }
 
-}
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let watKhwang = CLLocation(latitude: 16.137093, longitude: 100.302658)
+
+        let annotationInWatKhwang = MKPointAnnotation()
+        annotationInWatKhwang.coordinate = watKhwang.coordinate
+        annotationInWatKhwang.title = "Wat Khwang"
+        annotationInWatKhwang.subtitle = "Thailand"
+
+        myMapOUTLET.addAnnotation(annotationInWatKhwang)
+        myMapOUTLET.centerCoordinate = annotationInWatKhwang.coordinate
+
+    }
+
+
+}
